dff21cdca571787b479c70c934e16f83cc80e63d
Justin Klotz <jjklotz@oakland.edu>
2023-10-02 13:06:17 -0400

    Change

--- Naomotion/test.py
+++ Naomotion/test.py
@@ -5,7 +5,7 @@ import webbrowser
 import time
  
 time.sleep(1)
-webbrowser.open("https://imandissy.github.io/")
+webbrowser.open("https://jjklotz3.github.io/")
 
 #Delay to wait for page to load
 time.sleep(5)
